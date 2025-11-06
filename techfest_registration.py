82862623516314f5054ef98a02f9ed75b5f22f7a
Herreros <12413409@benilde.edu.ph>
2025-11-06 12:29:42 +0800

    Collect Participant Information

--- techfest_registration.py
+++ techfest_registration.py
@@ -7,3 +7,13 @@ num_p = int (input("How many participants will register?"))
 if num_p <= 0:
     print("Invalid number of participants")
     sys.exit()
+
+participants = []
+for x in range(num_p):
+    name = input("Enter participant name: ")
+    track = input("Enter chosen track: ")
+    participants.append({"name": name, "track": track})
+
+    print("Registered participants:")
+    for participant in participants:
+        print(participant["name"], participant["track"])
