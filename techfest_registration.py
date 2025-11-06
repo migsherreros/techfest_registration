173ad3071a7fdd9cbe59458ba9531eceeebe54f8
Herreros <12413409@benilde.edu.ph>
2025-11-06 12:22:41 +0800

    Implemented Participant Registration

--- techfest_registration.py
+++ techfest_registration.py
@@ -3,8 +3,7 @@ import sys
 print("Welcome to SMIT TechFest!")
 print("Event organized by Miguel Herreros of APPDAET BTCS2")
 
-p = input("How many participants will register?")
-if int(p) <= 0:
+num_p = int (input("How many participants will register?"))
+if num_p <= 0:
     print("Invalid number of participants")
     sys.exit()
-
